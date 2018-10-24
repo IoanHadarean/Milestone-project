d72b258f67749baaf4cd4791e22db6cb632b88c7
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-10-24 10:54:04 +0000

    Fixed sending emails function on the contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,15 @@
 function sendMail(form) {
-    emailjs.send("gmail", "dragon_ball"), {
-        "from_name": form.name.value,
-        "from_email": form.emailaddress.value,
-        "project_request": form.projectsummary.value
-    }
-    .then(
-        function(response){
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
+    emailjs.send("gmail", "dragon_ball", {
+            "from_name": form.name.value,
+            "from_email": form.emailaddress.value,
+            "project_request": form.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log(response);
+            },
+            function(error) {
+                console.log(error);
+            });
     return false;
 }
